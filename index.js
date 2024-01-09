a643ab1a12248cf3fc0c1c50fc6e8f85d7345aa4
Vivek-D-Joshi <vivekjoshib2b@gmail.com>
2024-01-09 14:33:12 +0530

    feat: create a server

---
+++ index.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+dotenv.config({ path: "./config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}...`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json("Hello from server...");
+});
